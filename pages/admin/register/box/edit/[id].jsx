e7426ecb393636f66e0097d4b4ffe484f549e45a
batkhishigt <batkhishigt.u@gmail.com>
2022-08-09 11:33:32 +0800

    box edit

--- pages/admin/register/box/edit/[id].jsx
+++ pages/admin/register/box/edit/[id].jsx
@@ -1,7 +1,7 @@
 import Layout from "components/Layouts/admin";
 import Tab from "components/Base/Tab/index";
-import { useRouter } from "next/router";
-import CreateAndEditForm from "/components/box/createAndEditForm";
+// import { useRouter } from "next/router";
+import CreateAndEditForm from "../../../../../components/Box/createAndEditForm";
 const Index = () => {
   return (
     <Layout>
