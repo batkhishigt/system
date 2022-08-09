7710bb7beac13a2e4bfc897cb4867594433b2a74
batkhishigt <batkhishigt.u@gmail.com>
2022-08-09 11:42:23 +0800

    delete bob edit

--- pages/admin/register/box/edit/[id].jsx
+++ pages/admin/register/box/edit/[id].jsx
@@ -1,13 +1,13 @@
 import Layout from "components/Layouts/admin";
 import Tab from "components/Base/Tab/index";
 // import { useRouter } from "next/router";
-import CreateAndEditForm from "../../../../../components/Box/createAndEditForm";
+// import CreateAndEditForm from "../../../../../components/Box/createAndEditForm";
 const Index = () => {
   return (
     <Layout>
       <div>
         <Tab />
-        <CreateAndEditForm isCreate={false} />
+        {/* <CreateAndEditForm isCreate={false} /> */}
       </div>
     </Layout>
   );
